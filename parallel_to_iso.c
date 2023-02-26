a8b5a0c3a556395b512d4005177fd97a6675d471
B.R.O.L.Y <114589218+RIDWANE-EL-FILALI@users.noreply.github.com>
2023-02-26 17:51:17 +0100

    deleted the second branch

--- parallel_to_iso.c
+++
@@ -1,7 +0,0 @@
-#include "fdf.h"
-
-void isometric(t_dot *point, double angle)
-{
-    point->x = (point->x - point->y) * cos(angle);
-    point->y = (point->x + point->y) * sin(angle) - point->z;
-}
